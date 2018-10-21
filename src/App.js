58c005e77bb43b3440263abef976ef9072cbbbb2
Santiago Topham <santitopham@gmail.com>
2018-10-20 22:23:30 -0300

    Registro conectando con Node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RegisterForm from './components/registerForm';
+import LoginForm from './components/loginForm';
+
+const ipServidor = 'localhost';
+const port = 3000;
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      companyTypes: [],
+      userTypes: []
+    }
+
+    this.registroUsuarioEmpresa = this.registroUsuarioEmpresa.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  componentDidMount(){
+    fetch(`http://${ipServidor}:${port}/api/empresas/types`)
+      .then(res => {
+        res.json()
+          .then(data => {
+            this.setState({companyTypes: data});
+          })
+      });
+
+      fetch(`http://${ipServidor}:${port}/api/user/types`)
+      .then(res => {
+        res.json()
+          .then(data => {
+            this.setState({userTypes: data});
+          })
+      });
+  }
+
+  registroUsuarioEmpresa(registerData){
+    let request = new Request(`http://${ipServidor}:${port}/api/register`, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json'}),
+      body: JSON.stringify(registerData)
+    });
+
+    fetch(request)
+      .then((res) => {
+          res.json()
+              .then(data => {
+                  console.log(data);
+              });
+      });
+  }
+
+  login(loginData){
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RegisterForm companyTypes={this.state.companyTypes} userTypes={this.state.userTypes} onClick={this.registroUsuarioEmpresa}/>
+        <br />
+        <LoginForm />
       </div>
     );
   }
---
+++ src/components/loginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.login = this.login.bind(this);
+    }
+
+    login(event){
+        event.preventDefault();
+        alert('llegue');
+    }
+    
+    render(){
+        return(
+            <form ref='loginUsuarios'>
+                <label ref='userEmail'>Email: </label>
+                <input type='text' ref='userEmail' placeholder='Email' />
+                <br />
+                <label ref='userPassword'>Contraseña: </label>
+                <input type='text' ref='userPassword' placeholder='Contraseña' />
+                <br />
+                <button onClick={this.login}>Iniciar sesion</button>
+            </form>
+        );
+    }
+}
+
+export default LoginForm;
